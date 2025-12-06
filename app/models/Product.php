e08406060e1bd66f702d7c021aadf006fc4dd8e4
willyvn <162080278+willyvn@users.noreply.github.com>
2025-12-06 15:08:12 +0700

    Add methods to get all products and count

--- app/models/Product.php
+++ app/models/Product.php
@@ -12,3 +12,30 @@ class Product {
         $stmt->close();
         return $product;
     }
+
+    /**
+     * Get all products with pagination
+     */
+    public static function getAll($db, $page = 1, $perPage = 10) {
+        $offset = ($page - 1) * $perPage;
+        $sql = "SELECT * FROM products ORDER BY id DESC LIMIT $perPage OFFSET $offset";
+        $result = $db->query($sql);
+        
+        $products = [];
+        if ($result) {
+            while ($row = $result->fetch_assoc()) {
+                $products[] = $row;
+            }
+        }
+        
+        return $products;
+    }
+
+    /**
+     * Get total count of products
+     */
+    public static function getCount($db) {
+        $result = $db->query('SELECT COUNT(*) AS cnt FROM products');
+        return ($result && $row = $result->fetch_assoc()) ? (int)$row['cnt'] : 0;
+    }
+}
